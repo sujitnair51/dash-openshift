b53162deab101e57fd926cd3c2c25f406ddfbfed
sujitnair51 <39463984+sujitnair51@users.noreply.github.com>
2022-05-11 17:43:43 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -41,7 +41,7 @@ app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
 )
 app.title = "DO Optimization"
-# server = app.server
+server = app.server
 application = app.server
 
 # Create controls
